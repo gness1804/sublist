6efc00b448892d506d05cba776fc162f1317ff4a
gness1804 <graham.nessler@gmail.com>
2022-05-17 09:27:55 -0500

    tests passing for equal length lists

--- sublist.ts
+++ sublist.ts
@@ -6,14 +6,29 @@ export class List {
     this.list = [...args];
   }
 
-  compare(listB: List): Comparison {
-    if (!this.list.length) {
-      if (!listB.list.length) return 'equal';
+  compare(secondList: List): Comparison {
+    const listA = this.list;
+    const listB = secondList.list;
+    if (!listA.length) {
+      if (!listB.length) return 'equal';
       return 'sublist';
     }
-    // this.list has a length of 1+
-    if (!listB.list.length) return 'superlist';
+    // listA has a length of 1+
+    if (!listB.length) return 'superlist';
     // both lists have a length of 1+
+    if (listA.length > listB.length) {
 
+    } else if (listA.length < listB.length) {
+
+    } else {
+      // this list lengths are the same.
+      let failedCount = 0;
+      listB.forEach((number, i) => {
+        if (number !== listA[i]) {
+          failedCount++;
+        }
+      })
+      return !!failedCount ? 'unequal' : 'equal';
+    }
   }
 }
